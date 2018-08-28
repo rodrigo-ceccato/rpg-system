d0e403ffc38f982da29663ac9df7ab6b041ee130
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2018-08-28 18:31:41 -0300

    updated mapview placeholder with coordinate system logic

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerDataService } from '../player-data.service';
 
 @Component({
   selector: 'app-map-view',
@@ -7,15 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapViewComponent implements OnInit {
 
-  public map = [[1,2],[3,4]];
+  public map = [[]];
   public selectedTile = {
     i: '0',
     j: '0'
   };
 
-  constructor() { 
-    
-  }
+  public tableW = 4;
+  public tableH = 4;
+
+  //need to remeber where player was,
+  //so we can remove when moving
+  playerLastPositionI = '0';
+  playerLastPositionJ = '0';
+
+  constructor(public Player:PlayerDataService) {  }
 
   ngOnInit() {
   }
@@ -24,5 +31,57 @@ export class MapViewComponent implements OnInit {
     this.selectedTile.i = i;
     this.selectedTile.j = j;
   }
+
+  generateMap(){
+    let newMap = new Array();
+
+    for (let i = 0; i < this.tableH; i++) {
+      newMap[i] = new Array();
+
+      for(let j = 0; j < this.tableW; j++){
+        newMap[i][j] = {
+          'blocked': false,
+          'inVision': []
+        };
+      }
+    }
+
+    this.map = newMap;
+  }
+
+  moveMe(){
+    let i = this.selectedTile.i;
+    let j = this.selectedTile.j;
+
+    //move the player
+    if(!this.map[i][j].inVision) {
+      this.map[i][j].inVision = [];
+    }
+
+    this.map[i][j].inVision.push(this.Player.player.name);
+
+    //remove from last position
+    let playerNametoBeMoved = this.Player.player.name;
+    let targetTile = this.map[this.playerLastPositionI][this.playerLastPositionJ].inVision;
+    let indexOfPlayerOnPreviusTile =  targetTile.indexOf(playerNametoBeMoved);
+
+    if(indexOfPlayerOnPreviusTile != -1) {
+      targetTile.splice(indexOfPlayerOnPreviusTile, 1);
+    }
+
+
+    //update last position
+    this.playerLastPositionI = i;
+    this.playerLastPositionJ = j;
+  }
+
+  toggleBlockTerrain(){
+    let i = this.selectedTile.i;
+    let j = this.selectedTile.j;
+
+    this.map[i][j].blocked = !this.map[i][j].blocked;
+  }
+
+ 
 
 }
