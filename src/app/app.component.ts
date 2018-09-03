ad35757909e221b38a8b1d171dc664abb2830d81
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2018-09-03 15:00:39 -0300

    started versioning the project according to Semantic Versioning 2.0.0, we are starting at version 0.2.0, the first 0 is a major version indicator, 2 is the minor indicator, witch will increase with each feature added, and the last number is the bug fix of the current patch. we get the version from the package-jason version attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RPG System';
+
+  public version: string = environment.VERSION;
 
   public generalView = true;
   public inventoryView = true;
